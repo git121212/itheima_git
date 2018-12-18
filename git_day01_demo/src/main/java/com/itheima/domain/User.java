821691805d04c374beb304ff534b7f3165f11586
mygit <bos@itcast.cn>
2018-12-18 21:39:54 +0800

    新增属性4

--- git_day01_demo/src/main/java/com/itheima/domain/User.java
+++ git_day01_demo/src/main/java/com/itheima/domain/User.java
@@ -4,30 +4,7 @@ public class User {
     private String name;
     private Integer age;
 
-    //新增
-    private String sex;
+    private String address;
 
-    public String getName() {
-        return name;
-    }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public String getSex() {
-        return sex;
-    }
-
-    public void setSex(String sex) {
-        this.sex = sex;
-    }
 }
